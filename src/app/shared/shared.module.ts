3c56dfeafdbe6c4ca3bbad5709f21bb87026f4cb
Matias De Stefano <matiasdestefano97@gmail.com>
2022-01-18 18:27:43 -0300

    Estructura del proyecto

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from './footer/footer.component';
+import { MenuComponent } from './menu/menu.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+
+
+@NgModule({
+  declarations: [
+    FooterComponent,
+    MenuComponent,
+    SidebarComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
